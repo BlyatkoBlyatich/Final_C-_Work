f4c3cacf112e74ec2bfa5e5c417f9f452acf772b
Василий Виссаров <vissar2016@rambler.ru>
2022-12-04 17:57:47 +0300

    Program added

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string [] array1 = new string[5] {"123", "2", "word", ":-)", "ByeBye"};
+string [] array2 = new string[array1.Length];
+
+void MyArray(string[] array1, string[] array2)
+{
+    int size = 0;
+    for(int i = 0; i < array1.Length; i ++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[size] = array1[i];
+            size++;
+        }
+    }
+}
+
+void ShowMyArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i ++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+MyArray(array1, array2);
+ShowMyArray(array2);
+
+
+
+
+
+
+
+
+
